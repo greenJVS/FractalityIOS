d66e374e33d5f12d6f6dec74566902e0be913954
greenJVS <green.jvs@gmail.com>
2019-03-21 20:49:29 +0300

    FractalCore Initial Commit

---
+++ FractalCore/FractalCore/FractalCore.h
@@ -0,0 +1,19 @@
+//
+//  FractalCore.h
+//  FractalCore
+//
+//  Created by Admin on 21/03/2019.
+//  Copyright © 2019 GreenJVS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FractalCore.
+FOUNDATION_EXPORT double FractalCoreVersionNumber;
+
+//! Project version string for FractalCore.
+FOUNDATION_EXPORT const unsigned char FractalCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FractalCore/PublicHeader.h>
+
+
